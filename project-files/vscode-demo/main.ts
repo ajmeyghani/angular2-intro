a7a757a3817ed465cc81e616be3974dd02794adf
st32lth <st32lth@gmail.com>
2016-01-03 02:02:34 -0500

    add code for vscode setup chapter.

---
+++ project-files/vscode-demo/main.ts
@@ -0,0 +1,3 @@
+const sum = (a: number, b: number): number => a + b;
+const r = sum(1,2);
+console.log(r);
---
+++ project-files/vscode-demo/output/main.js
@@ -0,0 +1,4 @@
+var sum = function (a, b) { return a + b; };
+var r = sum(1, 2);
+console.log(r);
+//# sourceMappingURL=main.js.map
